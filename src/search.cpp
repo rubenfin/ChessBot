72e57bfed5c1768c62a7a59196f5d2a012a9b628
Michael Moser <Michael.Moser11@web.de>
2025-03-14 12:52:25 +0100

    implement move_order try

--- src/search.cpp
+++ src/search.cpp
@@ -5,7 +5,10 @@
 #include "../include/logger.hpp"
 
 
+#include <cstddef>
 #include <limits.h>
+
+std::unordered_map<uint64_t, std::pair<size_t, struct move>> transitionTable;
 
 struct search_result alpha_beta(int alpha, int beta, int depth, const struct position *pos) {
 	struct search_result result;
@@ -18,13 +21,19 @@ struct search_result alpha_beta(int alpha, int beta, int depth, const struct pos
 		struct move moves[MAX_MOVES];
 		size_t count = generate_legal_moves(pos, moves);
 
-		for (size_t index = 0; index < count; ++index) {
+		const auto& it = transitionTable.find(hash);
+		if (it != transitionTable.end()) {
+			moves[it->second.first] = moves[0];
+			moves[0] = it->second.second;
+			transitionTable.erase(it);
+		}
+
+		for (size_t index = 0;index < count; ++index) {
 
 			struct position copy = *pos;
 			do_move(&copy, moves[index]);
 
 			int score = -alpha_beta(-beta, -alpha, depth -1, &copy).score;
-
 
 			if (score > alpha) {
 				alpha = score;
@@ -32,6 +41,7 @@ struct search_result alpha_beta(int alpha, int beta, int depth, const struct pos
 			if (score > result.score) {
 				result.move = moves[index];
 				result.score = score;
+				transitionTable[hash]= {index, moves[index]};
 			}
 
 			if (alpha >= beta)
